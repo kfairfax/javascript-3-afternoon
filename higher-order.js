99e53d0a4db1f3a4c33f59ac0e2d7540fa3a11b0
Katrina Fairfax <fairfaxkatrina@gmail.com>
2018-05-13 18:38:48 -0600

    completed higher-order functions

--- higher-order.js
+++ higher-order.js
@@ -20,8 +20,9 @@ const mixedNumbers = [6,3,1,7,5,2,6,8,9,4,2,7,9,3,1,8,4,3];
   (element, index, wholeArray)=>{}    Arrow Form
 */
 
-//Code Here
-let evenNumbers // = mixedNumbers.filter(/* Provide Your Callback Here */)
+let evenNumbers = mixedNumbers.filter(function(element, index, wholeArray){
+  return element%2===0;
+})
 
 
 
@@ -44,7 +45,9 @@ const prices = [15.00, 23.00, 78.00, 34.00, 12.00, 86.00, 12.00, 79.00, 32.00];
 */
 
 //Code Here
-let postTaxPrices // = prices.map(/* Provide Your Callback Here );
+let postTaxPrices = prices.map(function(element, index, wholeArray){
+  return element * 1.07;
+});
 
 
 
@@ -63,7 +66,10 @@ const populations = [8175133, 3792621, 2695598, 2100263];
 */
 
 //Code Here
-let totalPopulation //  = populations.reduce(/* Provide Your Callback Here */)
+let totalPopulation  = populations.reduce(function(runningTotal, curElement, curIndex, wholeArray){
+  
+  return (runningTotal + curElement);
+} )
 
 
 
@@ -89,7 +95,9 @@ const monstersInYourPocket = [{"monster":"Bulbabunny","CP":156},{"monster":"Bulb
 */
 
 //Code Here
-let myStrongest // = monstersInYourPocket.filter(/* Provide Your Callback Here */)
+let myStrongest = monstersInYourPocket.filter(function(val, i, arr){
+  return val["CP"] >200
+});
 
 
 
@@ -106,7 +114,9 @@ const orders = [{"price":15,"tax":0.09},{"price":42,"tax":0.07},{"price":56,"tax
   Use a higher order method to get the sum of all the order totals after adding in the sales tax
 */
 
-let ordersTotal //Code Here
+let ordersTotal = orders.filter(function(val, i, wholeArray){
+  return val["price"]*(val["tax"] +1);
+})
 
 
 
@@ -126,6 +136,14 @@ const purchases = [{"owner":"Barry","price":103},{"owner":"Bob","price":75},
   Use a high order method to create to get the sum of bobsTotal.
 */
 
-let bobsTotal //Code Here
+let bobsTotal = 0
+purchases.forEach(function(val, i, arr){
+  
+  
+  if(val["owner"]==="Bob"){
+      bobsTotal+=val["price"];
+  }
+})
+
 
 
